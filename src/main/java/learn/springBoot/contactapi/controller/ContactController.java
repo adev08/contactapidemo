5d3d7f15f7ae5203d3bb7506ee287055255baf50
alex mateo <vamateo85@gmail.com>
2024-01-06 21:14:52 -0500

    Added Contact controller

---
+++ src/main/java/learn/springBoot/contactapi/controller/ContactController.java
@@ -0,0 +1,49 @@
+package learn.springBoot.contactapi.controller;
+
+import java.net.URI;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import learn.springBoot.contactapi.domain.Contact;
+import learn.springBoot.contactapi.service.ContactService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
+public class ContactController {
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+        //return ResponseEntity.ok().body(contactService.createContact(contact));
+        return ResponseEntity.created(URI.create("/contacts/<user_ID>")).body(contactService.createContact(contact));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                     @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContacts(@PathVariable(value = "id") String id) {
+        return ResponseEntity.ok().body(contactService.getContact(id));
+    }
+
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id") String id, @RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().body(contactService.uploadPhoto(id, file));
+    }
+}
