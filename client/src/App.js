56e71efa16cd5548a85e0b9b59737ad96027ec38
alex mateo <vamateo85@gmail.com>
2024-03-08 18:32:51 -0500

    Added a dialog

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import ContactList from "./components/ContactList";
@@ -6,8 +6,28 @@ import { getContacts } from "./api/ContactService";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const modalRef = useRef();
+  const fileRef = useRef();
   const [data, setData] = React.useState({});
   const [currentPage, setCurrentPage] = React.useState(0);
+  const [file, setFile] = React.useState(undefined);
+  const [values, setValues] = React.useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    title: '',
+    status: '',
+  });
+
+  const onChange = (event) => {
+    setValues({...values, [event.target.name]: event.target.value});
+    console.log(values);
+  };
+
+ const handleNewContact = () => {
+
+ };
 
   const getAllContacts = async (page = 0, size = 10) => {
     try {
@@ -20,9 +40,7 @@ function App() {
     }
   };
 
-  const toogleModal = (show) => {
-    console.log("clicked");
-  };
+  const toggleModal = (show) => show ? modalRef.current.showModal() : modalRef.current.close();
 
   React.useEffect(() => {
     getAllContacts();
@@ -30,7 +48,7 @@ function App() {
 
   return (
     <>
-      <Header toogleModal={toogleModal} numbOfContacts={data.totalElements} />
+      <Header toggleModal={toggleModal} numbOfContacts={data.totalElements} />
       <main className="main">
         <div className="container">
           <Routes>
@@ -48,6 +66,103 @@ function App() {
           </Routes>
         </div>
       </main>
+
+      {/* Modal */}
+      <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Contact</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="divider"></div>
+        <div className="modal__body">
+          <form onSubmit={handleNewContact}>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input
+                  type="text"
+                  value={values.name}
+                  onChange={onChange}
+                  name="name"
+                  required
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input
+                  type="text"
+                  value={values.email}
+                  onChange={onChange}
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Title</span>
+                <input
+                  type="text"
+                  value={values.title}
+                  onChange={onChange}
+                  name="title"
+                  required
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input
+                  type="text"
+                  value={values.phone}
+                  onChange={onChange}
+                  name="phone"
+                  required
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Address</span>
+                <input
+                  type="text"
+                  value={values.address}
+                  onChange={onChange}
+                  name="address"
+                  required
+                />
+              </div>
+              <div className="input-box">
+                <span className="details">Account Status</span>
+                <input
+                  type="text"
+                  value={values.status}
+                  onChange={onChange}
+                  name="status"
+                  required
+                />
+              </div>
+              <div className="file-input">
+                <span className="details">Profile Photo</span>
+                <input
+                  type="file"
+                  onChange={(event) => setFile(event.target.files[0])}
+                  ref={fileRef}
+                  name="photo"
+                  required
+                />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button
+                onClick={() => toggleModal(false)}
+                type="button"
+                className="btn btn-danger"
+              >
+                Cancel
+              </button>
+              <button type="submit" className="btn">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </dialog>
     </>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Header = ({ toogleModal, numbOfContacts }) => {
+const Header = ({ toggleModal, numbOfContacts }) => {
   return (
     <header className="header">
       <div className="container">
         <h3>Contact List ({numbOfContacts})</h3>
-        <button onClick={() => toogleModal(true)} className="btn">
+        <button onClick={() => toggleModal(true)} className="btn">
           <i className="bi bi-plus-square"></i> Add New Contact
         </button>
       </div>
